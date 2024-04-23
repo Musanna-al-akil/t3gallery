5542148a0ac3b0fa804da21e41b4e1dce1715345
Musanna Al Akil <musannaal@gmail.com>
2024-04-23 19:03:48 +0600

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
-
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/13287447-b552-47b3-80a9-d925ccbe7cf5-tgpa6w.png",
+    "https://utfs.io/f/f51fa593-fc43-45f0-9402-a25a44961e44-44ihg8.png",
+    "https://utfs.io/f/79247722-2127-4ea3-974c-e16a87f5bd76-k182tr.jpeg",
+    "https://utfs.io/f/2a207b26-3caf-4855-a759-be89a78cfd2c-fwmi5k.png",
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>hello</h1>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages,...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
