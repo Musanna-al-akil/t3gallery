fa0b775679d743edc6afd05cf129737de21bfc05
Musanna Al Akil <musannaal@gmail.com>
2024-04-24 16:46:12 +0600

    use next/image

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 - [x] add authentication (w/ clerk)
 - [x] Add image upload
 - [x] "taint" (server-only)
-- [ ] Use Next/Image component
+- [x] Use Next/Image component
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page(parallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import {  getMyImages } from "~/server/queryies";
+import Image from "next/image";
+import { getMyImages } from "~/server/queryies";
 
 export const dynamic = "force-dynamic";
 
@@ -22,10 +23,17 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="w-48">
-          <img src={image.url} />
+        <div key={image.id} className="flex w-48 h-48 flex-col">
+          <Image
+            src={image.url}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+            // fill
+            alt="{image.name}"
+          />
           <div>{image.name}</div>
         </div>
       ))}
