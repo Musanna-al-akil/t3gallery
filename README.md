d7629b05a9b2e1b5702d08544206236a4d6acd4e
Musanna Al Akil <musannaal@gmail.com>
2024-04-24 15:43:23 +0600

    image upload added

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 - [x] Actually set up a database (vercel postgres)
 - [x] Attach databsase to UI
 - [x] add authentication (w/ clerk)
-- [ ] Add image upload
+- [x] Add image upload
 - [ ] "taint" (server-only)
 - [ ] Use Next/Image component
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,36 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
-export default  function HomePage() {
-
+export default function HomePage() {
   return (
     <main className="">
       <SignedOut>
-        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
       </SignedOut>
       <SignedIn>
-        <Images/>
+        <Images />
       </SignedIn>
-      
     </main>
   );
 }
 
-async function Images(){
-    
+async function Images() {
   const images = await db.query.images.findMany({
-    orderBy:(model,{desc})=> desc(model.id)
+    orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
     <div className="flex flex-wrap gap-4">
-    {[...images, ...images].map((image,index) => (
-      <div key={image.id + '-'+ index } className="w-48">
-        <img src={image.url} />
-        <div>{image.name}</div>
-      </div>
-    ))}
-  </div>
-  )
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
